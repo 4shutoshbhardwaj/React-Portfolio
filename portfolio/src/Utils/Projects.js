17fb26e40902d2edad967a0bfdaf3f10d8e607d3
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2022-11-12 22:52:16 +0530

    Done

---
+++ portfolio/src/Utils/Projects.js
@@ -0,0 +1,23 @@
+import flip from "../ash/flip.jpg"
+import reliance from "../ash/reliance.jpg"
+export const projects = [
+
+	{
+		img: flip,
+		title: 'Clone of Flipkart',
+		des: 'This project is a clone of famous e-commerce website of India Full flow is created in this website from signup to buying a product, This website is created on MERN technology with Material UI',
+		live: 'https://flipkart-clone-4shutoshbhardwaj.netlify.app/',
+		demo: '',
+		gitHub: 'https://github.com/4shutoshbhardwaj/MERN-Flipkart-Clone',
+		technologies: ["React","Redux","Express","MongoDB","NodeJs","Material UI"],
+	},
+	{
+		img: reliance,
+		title: 'Reliance Digital clone',
+		des: 'Reliance Digital is an Indian electronic products selling website and E-commerce platform for consumers Whole flow is created from login, signup till payment Just like original website. Its the exact identical website just like original one',
+		demo: '',
+		live: 'https://reliance-digital123.netlify.app/',
+		gitHub: 'https://github.com/4shutoshbhardwaj/Reliance-Digital-Clone-Project',
+		technologies: ['HTML', 'CSS', 'javaScript','GitHub','Json Server'],
+	}
+];
